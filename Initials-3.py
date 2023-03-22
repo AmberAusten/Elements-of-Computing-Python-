d3d0954dc7de860e9a443c61cc466afc54b49f4d
AmberAusten <newsforamber@gmail.com>
2023-03-21 20:20:36 -0500

    Add files via upload

---
+++ Initials-3.py
@@ -0,0 +1,28 @@
+#  File: Initials.py
+#  Description: Programming assignment 1
+#  Student's Name: K.Amber Vasquez  
+#  Student's UT EID: kav835
+#  Course Name: CS 303E 
+#  Unique Number: 50180
+#
+#  Date Created:1/24/20
+#  Date Last Modified:1/28/20
+
+def main ():
+
+    print("")
+    print("   KKKK    KKKK            AAAA            VVVV    VVVV   ")
+    print("   KKKK    KKKK            AAAA            VVVV    VVVV   ")
+    print("   KKKK    KKK            AA  AA            VVV    VVV   ")
+    print("   KKKK    KK             AA  AA            VVV    VVV   ")
+    print("   KKKK  KK              AAAAAAAA            VV    VV   ") 
+    print("   KKKK KK               AAAAAAAA            VV    VV   ")
+    print("   KKKK KKK             AA      AA            VV  VV   ")
+    print("   KKKK  KKKK           AA      AA            VV  VV   ")
+    print("   KKKK   KKKK    ..   AA        AA   ..       V  V       ..   ")
+    print("   KKKK    KKKK   ..   AA        AA   ..        VV        ..   ")
+    print("")
+
+main()
+
+    

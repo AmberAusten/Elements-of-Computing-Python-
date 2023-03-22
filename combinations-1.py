55d29799952b696ab3ae3653d36d6bf812adee92
AmberAusten <newsforamber@gmail.com>
2023-03-21 20:43:58 -0500

    Add files via upload

---
+++ combinations-1.py
@@ -0,0 +1,43 @@
+#  File: Combinations.py
+#  Description: A program that prints a table listing the
+#               number of possible hands of r cards, r ranging from 0 to 52.
+#  Student's Name: K. Amber Vasquez
+#  Student's UT EID: kav835
+#  Course Name: CS 303E 
+#  Unique Number: 50180
+#
+#  Date Created: March 9, 2020
+#  Date Last Modified: March 12, 2020
+
+   
+
+def factorial(num):
+
+    fact = 1
+    
+    for i in range (1, num + 1):
+        fact = fact * i
+        
+    return fact
+
+def combinations (n,r):
+
+        return factorial(n)//(factorial(r)*factorial(n-r))
+
+
+def main ():
+
+    n = 52
+    
+    print ()
+    print (format("Cards","<1s"),format("Combinations",">16s"))
+    print ("="*22)
+    
+    for i in range (0,n+1):
+        print(format(i,">3d"),format(combinations(n,i),">18d"))
+        
+    print ()
+
+    
+
+main ()
